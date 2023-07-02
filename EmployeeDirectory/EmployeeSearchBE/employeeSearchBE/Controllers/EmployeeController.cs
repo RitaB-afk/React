3eaca9dd017e3b1d6423ce13094b119aa17bbf4b
RitaB-afk <77437092+RitaB-afk@users.noreply.github.com>
2023-07-02 14:46:06 +0200

    Add files via upload

---
+++ EmployeeDirectory/EmployeeSearchBE/employeeSearchBE/Controllers/EmployeeController.cs
@@ -0,0 +1,36 @@
+﻿using employeeSearchBE.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace employeeSearchBE.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : Controller
+    {
+        List<Employee> employees = new List<Employee>();
+       public EmployeeController()
+        {
+            this.employees.Add(new Employee(1, "Developer", "Romin Irani", "408-1234567"));
+            this.employees.Add(new Employee(2, "Developer", "Neil Irani", "408-1111111"));
+            this.employees.Add(new Employee(3, "Program Directory", "Tom Hanks", "408-2222222"));
+
+        }
+
+        [HttpGet()]
+        public List<Employee> GetEmployees()
+        {
+            return this.employees;
+        }
+
+        [HttpGet("name")]
+        public List<Employee> searchEmployees(string name)
+        {
+
+            var result = this.employees.Where(employees => employees.Name.Contains(name)).ToList();
+            return result;
+        }
+
+
+
+    }
+}
---
+++ EmployeeDirectory/EmployeeSearchBE/employeeSearchBE/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿namespace employeeSearchBE.Models
+{
+    public class Employee
+    {
+        public Employee(int id, string jobTitleName, string name, string phoneNumber)
+        {
+            Id = id;
+            JobTitleName = jobTitleName;
+            Name = name;
+            PhoneNumber = phoneNumber;
+        }
+
+        public int Id { get; set; }
+        public string JobTitleName { get; set; }
+        public string Name { get; set; }
+       
+        public string PhoneNumber { get; set; }
+
+    }
+}
