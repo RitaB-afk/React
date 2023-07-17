7d4d65e676f3d36944a05863b160769074bdd062
RitaB-afk <77437092+RitaB-afk@users.noreply.github.com>
2023-07-17 22:54:37 +0200

    Add files via upload

---
+++ quizApp/quizAPI/Models/QuizDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Quiz_API.Models
+{
+    public class QuizDbContext: DbContext
+    {
+        public QuizDbContext()
+        {
+
+        }
+        public QuizDbContext(DbContextOptions<QuizDbContext> options): base(options)
+        {
+
+
+        }
+
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Participant> Participants { get; set; }
+
+    }
+}
